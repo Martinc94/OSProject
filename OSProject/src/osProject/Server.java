df29b8c5c49fa0def156859754c15fe4f0440fbe
Martin Coleman <Martinc94@live.co.uk>
2016-01-03 20:24:45 +0000

    started on server

--- OSProject/src/osProject/Server.java
+++ OSProject/src/osProject/Server.java
@@ -1,6 +1,8 @@
 package osProject;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
@@ -9,23 +11,73 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+	static Map<String,String> loginMap = new ConcurrentHashMap<String,String>();
+	static int id = 0;
+	
   public static void main(String[] args) throws Exception {
 	//creates a new serverSocket on port 2004 
-    ServerSocket m_ServerSocket = new ServerSocket(2004,10);
-    int id = 0;
-    //loop that accepts client and starts a new thread
+	ServerSocket socket = new ServerSocket(2004,10);	
+		
+    //read login from file 
+    parseLogin();
+    
+  //loop that accepts client and starts a new thread
     while (true) {
       //blocking method that waits for connection
-      Socket clientSocket = m_ServerSocket.accept();
+      Socket clientSocket = socket.accept();
       //creates new thread with socket and new id
       ClientServiceThread cliThread = new ClientServiceThread(clientSocket, id++);
       //starts thread
       cliThread.start();
-    }
-  }
+    }//end while
+    
+  }//end main
+  
+  //METHOD/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+  
+ 
+  
+  static void parseLogin() throws NumberFormatException, IOException {
+		String fileName="users.txt";
+		BufferedReader br = null;
+		
+		br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+		
+		String next;
+		
+		
+		
+		while((next = br.readLine())!= null){
+			//split the string
+			String[] split = next.split(" ");
+			
+			System.out.println(split[0]);
+			System.out.println(split[1]);
+			
+			String username = split[0];
+			String password = split[1];
+			
+			//pass to new login map
+			loginMap.put(split[0], split[1]);
+			
+			
+		}//while
+		
+		
+	}
+  
+  
+  
+  
 }
+
+
+
 
 class ClientServiceThread extends Thread {
 //variables
@@ -85,6 +137,11 @@ class ClientServiceThread extends Thread {
     }//end catch
     
   }//end run
+  
+  void checkLogin(){
+	//compare to username and password
+	  
+  }
   
 }//end ClientServiceThread
 
